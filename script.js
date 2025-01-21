bb0c0a94d8c9271820249cb8d5d32039d4c54518
Prajwal Kumar <94397677+prajwalyatharth@users.noreply.github.com>
2025-01-21 23:39:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor (species){
+		this.species = species;
+	}
+	get species(){
+		return this.species;
+	}
+	makeSound(){
+		console.log(`The ${this.species} makes a sound`);
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+    constructor(species) {
+        super(species); // Call the parent class constructor
+    }
 
-class Cat extends Animal {}
+    // Method specific to Dog
+    bark() {
+        console.log("woof");
+    }
+}
+
+class Cat extends Animal {
+	constructor(species) {
+        super(species); // Call the parent class constructor
+    }
+
+    // Method specific to Cat
+    purr() {
+        console.log("purr");
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
